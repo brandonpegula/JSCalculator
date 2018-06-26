2682cb1c1524f61e204e26c0356375ce49657dc9
Brandon P <brandonpegula@gmail.com>
2018-06-26 17:56:24 -0400

    formatting changes

--- calc.js
+++ calc.js
@@ -2,3 +2,19 @@ function insert(num){
     document.form.textview.value = document.form.textview.value+num
     
 };
+
+function equal(){
+    var equation = document.form.textview.value;
+    if (equation) {
+        document.form.textview.value = eval(equation)
+    }
+};
+
+function clean(){
+    document.form.textview.value = "";
+};
+
+function back(){
+    var equation = document.form.textview.value;
+    document.form.textview.value = equation.substring(0,equation.length-1);
+};
