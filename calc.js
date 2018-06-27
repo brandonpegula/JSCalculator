ce17773351b0560a4adc7204f11bfe32c500e0a4
Brandon P <brandonpegula@gmail.com>
2018-06-27 15:08:52 -0400

    Working on Percent

--- calc.js
+++ calc.js
@@ -1,20 +1,21 @@
-function insert(num){
+function insert(num){ //Function to insert selected number or value.
     document.form.textview.value = document.form.textview.value+num
     
 };
 
-function equal(){
+function equal(){ //Function to solve entered equation.
     var equation = document.form.textview.value;
     if (equation) {
         document.form.textview.value = eval(equation)
     }
 };
 
-function clean(){
+function clean(){ //Function to reset calculator.
     document.form.textview.value = "";
 };
 
-function back(){
+function back(){ //Function to move one space back in textview.
     var equation = document.form.textview.value;
     document.form.textview.value = equation.substring(0,equation.length-1);
 };
+
